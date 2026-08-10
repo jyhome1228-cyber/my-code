fffc44cf2ab7d57ddf93bb04d8760b19a1719360
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-10 18:54:35 +0900

    add optional short image URL base config

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -8,3 +8,8 @@ window.MYCODE_FIREBASE_CONFIG = {
   messagingSenderId: "268885401102",
   appId: "1:268885401102:web:9c7608b60f41002262265f"
 };
+
+// Leave blank until Firebase Hosting + the /i/:shortCode redirect function are deployed.
+// After deployment, set this to e.g. "https://mycode-web.web.app" and the UI will
+// automatically prefer short image addresses such as /i/Ab3xK7 over the long Storage URL.
+window.MYCODE_SHORT_BASE = "";
