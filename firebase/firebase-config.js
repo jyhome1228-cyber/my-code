03c91410499d3f1467ecff2236dc665c1ffa0514
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-10 13:41:26 +0900

    connect mycode-web Firebase config

---
+++ firebase/firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Web App configuration for my:code.
+// This configuration identifies the Firebase project; access is protected by Auth + Security Rules.
+window.MYCODE_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCKpU0EavwWyTy3BwoKeQlqDXKTK8287qo",
+  authDomain: "mycode-web.firebaseapp.com",
+  projectId: "mycode-web",
+  storageBucket: "mycode-web.firebasestorage.app",
+  messagingSenderId: "268885401102",
+  appId: "1:268885401102:web:9c7608b60f41002262265f"
+};
