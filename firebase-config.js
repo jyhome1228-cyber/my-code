101e199b983bb694d713c86d2e4f3e5ca6146f8e
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-17 13:05:50 +0900

    Connect My Code to Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,10 @@
-// Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정 및 구성에서
-// 아래 값들을 복사해서 교체하세요.
-// 이 파일의 Firebase 웹 config 값은 서버 비밀키가 아닙니다.
-// 실제 업로드 권한은 Firebase Storage Security Rules로 제한해야 합니다.
-
+// Firebase project configuration for 마이코드
+// Project: mycode-web
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCkpU0EavwWyTy3BwokEqLqDXKTK8287qo",
+  authDomain: "mycode-web.firebaseapp.com",
+  projectId: "mycode-web",
+  storageBucket: "mycode-web.firebasestorage.app",
+  messagingSenderId: "268885401102",
+  appId: "1:268885401102:web:9c7608b60f41002262265f"
 };
