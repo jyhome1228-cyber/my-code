5990da0bf2d6a3bf233610671e1dee30153fa033
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-18 00:40:23 +0900

    Add Firebase client config for MY CODE accounts

---
+++ firebase-client.js
@@ -0,0 +1,10 @@
+// MY CODE user account / library configuration
+// Firebase Web config values are public identifiers. Access control belongs in Firebase Auth + Firestore Security Rules.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCkpU0EavwWyTy3BwokEqLqDXKTK8287qo",
+  authDomain: "mycode-web.firebaseapp.com",
+  projectId: "mycode-web",
+  storageBucket: "mycode-web.firebasestorage.app",
+  messagingSenderId: "268885401102",
+  appId: "1:268885401102:web:9c7608b60f41002262265f"
+};
