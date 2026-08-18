0920584f1f1b06d557202eb2f762ddd57cbb39f0
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-18 16:49:17 +0900

    Simplify navigation to About and How to Use

--- site-nav.js
+++ site-nav.js
@@ -1,10 +1,10 @@
 (() => {
-  const FAVICON = './mycode-favicon-orange-20260818.svg?v=20260818-19';
+  const FAVICON = './mycode-favicon-orange-20260818.svg?v=20260818-21';
 
   if (!document.querySelector('link[data-mycode-header]')) {
     const headerStyle = document.createElement('link');
     headerStyle.rel = 'stylesheet';
-    headerStyle.href = './header-consistency.css?v=20260818-19';
+    headerStyle.href = './header-consistency.css?v=20260818-21';
     headerStyle.dataset.mycodeHeader = 'true';
     document.head.appendChild(headerStyle);
   }
@@ -17,7 +17,7 @@
   document.head.appendChild(icon);
   const shortcut = document.createElement('link');
   shortcut.rel = 'shortcut icon';
-  shortcut.href = FAVICON;
+  shortcut.href = './favicon.ico?v=20260818-21';
   document.head.appendChild(shortcut);
 
   let theme = document.querySelector('meta[name="theme-color"]');
@@ -31,8 +31,8 @@
   const nav = document.querySelector('.platform-nav');
   if (nav) {
     nav.innerHTML = `
-      <a href="./product.html">Product</a>
-      <a href="./workspace.html">Workspace</a>
+      <a href="./about.html">About</a>
+      <a href="./how-to-use.html">How to Use</a>
       <a href="./pricing.html">Pricing</a>
       <a href="./magazine.html">Magazine</a>
       <a href="./faq.html">FAQ</a>
@@ -41,7 +41,8 @@
 
   document.querySelectorAll('.company-footer-bottom > div').forEach((links) => {
     links.innerHTML = `
-      <a href="./product.html">Product</a>
+      <a href="./about.html">About</a>
+      <a href="./how-to-use.html">How to Use</a>
       <a href="./pricing.html">Pricing</a>
       <a href="./magazine.html">Magazine</a>
       <a href="./faq.html">FAQ</a>
